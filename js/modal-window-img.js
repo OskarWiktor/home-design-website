f83776a22097e715e40cc7a448bd9ebf14431056
Oskar Wiktor <oskar.wiktor3@wp.pl>
2023-06-07 13:53:38 +0200

    feat(portfolio): portfolio with modal windows/img

---
+++ js/modal-window-img.js
@@ -0,0 +1,15 @@
+const modal = document.getElementById('modal');
+const images = document.getElementsByClassName('portfolio__img');
+const modalImg = document.getElementById("img01");
+
+for (let i = 0; i < images.length; i++) {
+  let img = images[i];
+  img.onclick = function(evt) {
+    modal.style.display = "block";
+    modalImg.src = this.src;
+  }
+}
+
+modal.onclick = function() {
+  modal.style.display = "none";
+}
